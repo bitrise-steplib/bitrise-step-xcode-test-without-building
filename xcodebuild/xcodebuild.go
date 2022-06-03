0ad51cf2ec620d667607398379b3f38725a84332
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2022-06-03 14:09:33 +0200

    Update xcodebuild.go

--- xcodebuild/xcodebuild.go
+++ xcodebuild/xcodebuild.go
@@ -43,11 +43,6 @@ func New(logger log.Logger, commandFactory command.Factory, pathProvider pathuti
 }
 
 func (x xcodebuild) TestWithoutBuilding(xctestrun, destination, testRepetitionMode string, maximumTestRepetitions int, relaunchTestsForEachRepetition bool, opts ...string) (string, error) {
-	outputDir, err := x.createTestOutputDir(xctestrun)
-	if err != nil {
-		return "", err
-	}
-
 	logFile, err := x.createXcodebuildLogFile()
 	if err != nil {
 		return "", err
@@ -60,21 +55,12 @@ func (x xcodebuild) TestWithoutBuilding(xctestrun, destination, testRepetitionMo
 
 	outputWriter := io.MultiWriter(os.Stdout, logFile)
 
-	options := []string{"test-without-building", "-xctestrun", xctestrun, "-destination", destination, "-resultBundlePath", outputDir}
-	switch testRepetitionMode {
-	case TestRepetitionUntilFailure:
-		options = append(options, "-run-tests-until-failure")
-	case TestRepetitionRetryOnFailure:
-		options = append(options, "-retry-tests-on-failure")
+	outputDir, err := x.createTestOutputDir(xctestrun)
+	if err != nil {
+		return "", err
 	}
-	if testRepetitionMode != TestRepetitionNone {
-		options = append(options, "-test-iterations", strconv.Itoa(maximumTestRepetitions))
-	}
-	if relaunchTestsForEachRepetition {
-		options = append(options, "-test-repetition-relaunch-enabled", "YES")
-	}
-	options = append(options, opts...)
 
+	options := createXcodebuildOptions(xctestrun, destination, testRepetitionMode, maximumTestRepetitions, relaunchTestsForEachRepetition, outputDir)
 	cmd := x.commandFactory.Create("xcodebuild", options, &command.Opts{
 		Stdout: outputWriter,
 		Stderr: outputWriter,
@@ -83,6 +69,29 @@ func (x xcodebuild) TestWithoutBuilding(xctestrun, destination, testRepetitionMo
 	x.logger.TDonef(cmd.PrintableCommandArgs())
 	xcodebuildErr := cmd.Run()
 
+	return x.handleError(xcodebuildErr, outputDir, logFile)
+}
+
+func (x xcodebuild) createXcodebuildLogFile() (*os.File, error) {
+	tempDir, err := x.pathProvider.CreateTempDir("xcodebuild")
+	if err != nil {
+		return nil, err
+	}
+
+	return os.Create(path.Join(tempDir, "test-without-building.log"))
+}
+
+func (x xcodebuild) createTestOutputDir(xctestrun string) (string, error) {
+	tempDir, err := x.pathProvider.CreateTempDir("XCUITestOutput")
+	if err != nil {
+		return "", err
+	}
+
+	fileName := strings.TrimSuffix(filepath.Base(xctestrun), filepath.Ext(xctestrun))
+	return path.Join(tempDir, fmt.Sprintf("Test-%s.xcresult", fileName)), nil
+}
+
+func (x xcodebuild) handleError(xcodebuildErr error, outputDir string, logFile *os.File) (string, error) {
 	empty, err := isDirEmpty(outputDir)
 	if err != nil {
 		x.logger.Warnf("Failed to check if test result bundle is empty: %s", err)
@@ -112,23 +121,21 @@ func (x xcodebuild) TestWithoutBuilding(xctestrun, destination, testRepetitionMo
 	return outputDir, nil
 }
 
-func (x xcodebuild) createXcodebuildLogFile() (*os.File, error) {
-	tempDir, err := x.pathProvider.CreateTempDir("xcodebuild")
-	if err != nil {
-		return nil, err
+func createXcodebuildOptions(xctestrun, destination, testRepetitionMode string, maximumTestRepetitions int, relaunchTestsForEachRepetition bool, outputDir string, opts ...string) []string {
+	options := []string{"test-without-building", "-xctestrun", xctestrun, "-destination", destination, "-resultBundlePath", outputDir}
+	switch testRepetitionMode {
+	case TestRepetitionUntilFailure:
+		options = append(options, "-run-tests-until-failure")
+	case TestRepetitionRetryOnFailure:
+		options = append(options, "-retry-tests-on-failure")
 	}
-
-	return os.Create(path.Join(tempDir, "test-without-building.log"))
-}
-
-func (x xcodebuild) createTestOutputDir(xctestrun string) (string, error) {
-	tempDir, err := x.pathProvider.CreateTempDir("XCUITestOutput")
-	if err != nil {
-		return "", err
+	if testRepetitionMode != TestRepetitionNone {
+		options = append(options, "-test-iterations", strconv.Itoa(maximumTestRepetitions))
 	}
-
-	fileName := strings.TrimSuffix(filepath.Base(xctestrun), filepath.Ext(xctestrun))
-	return path.Join(tempDir, fmt.Sprintf("Test-%s.xcresult", fileName)), nil
+	if relaunchTestsForEachRepetition {
+		options = append(options, "-test-repetition-relaunch-enabled", "YES")
+	}
+	return append(options, opts...)
 }
 
 func isDirEmpty(name string) (bool, error) {
