efabb86a8fe2c6e083248ebf5edb4105d6ecf2e0
Szabolcs Toth <54896607+tothszabi@users.noreply.github.com>
2025-01-24 15:59:39 +0100

    Add tests

--- step/step_test.go
+++ step/step_test.go
@@ -1,6 +1,7 @@
 package step
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
@@ -16,6 +17,16 @@ func Test_GivenStep_WhenProcessConfig_ThenSplitsAdditionalOptions(t *testing.T) 
 	// Given
 	step, testingMocks := createStepAndMocks(t)
 
+	onlyTesting := []string{
+		"target1",
+		"target2/testClass1",
+		"target3/testClass1/testFunction",
+	}
+	skipTesting := []string{
+		"target4",
+		"target5/testClass1",
+		"target6/testClass1/testFunction",
+	}
 	inputs := map[string]string{
 		"xctestrun":                          "my_test.xctestrun",
 		"destination":                        "platform=iOS Simulator,name=iPhone 8 Plus,OS=latest",
@@ -23,6 +34,8 @@ func Test_GivenStep_WhenProcessConfig_ThenSplitsAdditionalOptions(t *testing.T) 
 		"maximum_test_repetitions":           "3",
 		"relaunch_tests_for_each_repetition": "no",
 		"xcodebuild_options":                 "-parallel-testing-enabled YES",
+		"only_testing":                       strings.Join(onlyTesting, "\n"),
+		"skip_testing":                       strings.Join(skipTesting, "\n"),
 	}
 	for key, value := range inputs {
 		testingMocks.envRepository.On("Get", key).Return(value)
@@ -39,13 +52,15 @@ func Test_GivenStep_WhenProcessConfig_ThenSplitsAdditionalOptions(t *testing.T) 
 	// Then
 	require.NoError(t, err)
 	require.Equal(t, []string{"-parallel-testing-enabled", "YES"}, config.XcodebuildOptions)
+	require.Equal(t, onlyTesting, config.OnlyTesting)
+	require.Equal(t, skipTesting, config.SkipTesting)
 }
 
 func Test_GivenStep_WhenXcodebuildFailsOnAutomaticRetryReason_ThenXcodebuildCommandRetried(t *testing.T) {
 	// Given
 	step, testingMocks := createStepAndMocks(t)
 
-	testingMocks.xcodebuild.On("TestWithoutBuilding", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return("", &xcodebuild.XcodebuildError{Log: "Test runner never began executing tests after launching."})
+	testingMocks.xcodebuild.On("TestWithoutBuilding", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return("", &xcodebuild.XcodebuildError{Log: "Test runner never began executing tests after launching."})
 	testingMocks.logger.On("Println").Return()
 	testingMocks.logger.On("Infof", mock.Anything).Return()
 	testingMocks.logger.On("Warnf", mock.Anything, mock.Anything).Return()
@@ -131,18 +146,18 @@ func createStepAndMocks(t *testing.T) (XcodebuildTester, testingMocks) {
 	inputParser := stepconf.NewInputParser(envRepository)
 	logger := new(mocks.Logger)
 	deviceFinder := mocks.NewDeviceFinder(t)
-	xcodebuild := new(mocks.Xcodebuild)
+	xcbuild := new(mocks.Xcodebuild)
 	outputExporter := new(mocks.OutputExporter)
-	step := NewXcodebuildTester(log.NewLogger(), inputParser, deviceFinder, xcodebuild, envRepository, outputExporter)
+	step := NewXcodebuildTester(log.NewLogger(), inputParser, deviceFinder, xcbuild, envRepository, outputExporter)
 
-	mocks := testingMocks{
+	m := testingMocks{
 		envRepository:  envRepository,
 		inputParser:    inputParser,
 		logger:         logger,
 		deviceFinder:   deviceFinder,
-		xcodebuild:     xcodebuild,
+		xcodebuild:     xcbuild,
 		outputExporter: outputExporter,
 	}
 
-	return step, mocks
+	return step, m
 }
---
+++ xcodebuild/xcodebuild_test.go
@@ -0,0 +1,49 @@
+package xcodebuild
+
+import (
+	"os"
+	"testing"
+
+	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-io/go-utils/v2/pathutil"
+	"github.com/bitrise-io/go-xcode/v2/destination"
+	"github.com/bitrise-steplib/bitrise-step-xcode-test-without-building/mocks"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTestConfiguration(t *testing.T) {
+	commandMock := new(mocks.Command)
+	commandMock.On("PrintableCommandArgs").Return("")
+	commandMock.On("Run").Return(nil)
+
+	params := []string{"test-without-building", "-xctestrun", "test.xctestrun", "-destination", "id=test-UDID", "-resultBundlePath", "/test/path/Test-test.xcresult", "-only-testing:target1", "-only-testing:target2/testClass1", "-only-testing:target3/testClass1/testFunction", "-skip-testing:target4", "-skip-testing:target5/testClass1", "-skip-testing:target6/testClass1/testFunction"}
+
+	factoryMock := new(mocks.Factory)
+	factoryMock.On("Create", "xcodebuild", params, mock.Anything).Return(commandMock, nil).Once()
+
+	pathProviderMock := new(mocks.PathProvider)
+	pathProviderMock.On("CreateTempDir", "xcodebuild").Return(os.TempDir(), nil).Once()
+	pathProviderMock.On("CreateTempDir", "TestOutput").Return("/test/path", nil).Once()
+
+	xcbuild := New(log.NewLogger(), factoryMock, pathProviderMock, pathutil.NewPathChecker())
+	device := destination.Device{ID: "test-UDID"}
+	onlyTesting := []string{
+		"target1",
+		"target2/testClass1",
+		"target3/testClass1/testFunction",
+	}
+	skipTesting := []string{
+		"target4",
+		"target5/testClass1",
+		"target6/testClass1/testFunction",
+	}
+
+	_, err := xcbuild.TestWithoutBuilding("test.xctestrun", onlyTesting, skipTesting, device, "none", 0, false)
+	require.NoError(t, err)
+
+	pathProviderMock.AssertExpectations(t)
+	commandMock.AssertExpectations(t)
+	factoryMock.AssertExpectations(t)
+}
