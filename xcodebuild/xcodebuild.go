6eaed8dd4eda1d10afc7f671ab60a0f67cc5ada7
Szabolcs Toth <54896607+tothszabi@users.noreply.github.com>
2025-03-02 14:22:27 +0100

    wip

--- xcodebuild/xcodebuild.go
+++ xcodebuild/xcodebuild.go
@@ -171,11 +171,11 @@ func createXcodebuildOptions(testPath string, onlyTesting, skipTesting []string,
 	}
 
 	if 0 < len(skipTesting) {
-		var args []string
+		var args string
 		for _, identifier := range skipTesting {
-			args = append(args, fmt.Sprintf("-skip-testing:%s", identifier))
+			args += fmt.Sprintf("-skip-testing:%s ", identifier)
 		}
-		options = append(options, args...)
+		options = append(options, args)
 	}
 
 	return append(options, opts...)
